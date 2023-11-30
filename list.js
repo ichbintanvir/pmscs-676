9d60b327b9a5dc0245588fac76a29297e5269111
ichbintanvir <tanvirnoyon.bubt@gmail.com>
2023-11-30 21:27:04 +0600

    add important links

---
+++ list.js
@@ -0,0 +1,113 @@
+const question = () => {
+    const list = [
+        {
+            Lecture: '1',
+            question_no: '1',
+            question: 'Draw the complete diagram of a communication system',
+            slide: '3'
+        },
+        {
+            Lecture: '1',
+            question_no: '2',
+            question: 'Explain information from intuitive point of view and derived the expression of information carried by composite message.',
+            slide: '[6,7]'
+        },
+        {
+            Lecture: '1',
+            question_no: '3',
+            question: 'Explain information from engineering point of view.',
+            slide: '8'
+        },
+        {
+            Lecture: '1',
+            question_no: '4',
+            question: 'What will happend if binary or gray code is used in a communication system.',
+            slide: '10 only first pera'
+        },
+        {
+            Lecture: '2',
+            question_no: '1',
+            question: 'Derive the expression of Entropy.',
+            slide: '16'
+        },
+        {
+            Lecture: '2',
+            question_no: '2',
+            question: 'Explain information rate with apporopiate diagram.',
+            slide: '18'
+        },
+        {
+            Lecture: '2',
+            question_no: '3',
+            question: 'For a binary source show that entropy is maximum where messages are equalprobable. Determain the maximum entropy.',
+            slide: '20'
+        },
+        {
+            Lecture: '2',
+            question_no: '4',
+            question: 'Problems on Shannon-Fano Algorithm.',
+            slide: '[26-31]'
+        },
+        {
+            Lecture: '3',
+            question_no: '1',
+            question: 'Big problem',
+            slide: '32'
+        },
+        {
+            Lecture: '3',
+            question_no: '2',
+            question: 'Define source coding. Compare lossless and lossy compression.',
+            slide: '[64 + own idea]'
+        },
+        {
+            Lecture: '3',
+            question_no: '3',
+            question: 'Problem on Huffman Coding.',
+            slide: '[70-73]'
+        },
+        {
+            Lecture: '3',
+            question_no: '4',
+            question: 'Problem on prefix code',
+            slide: '75'
+        },
+        {
+            Lecture: '3',
+            question_no: '5',
+            question: 'Problem on Lempel ziv encoding.',
+            slide: '[79-82]'
+        },
+        {
+            Lecture: '3',
+            question_no: '6',
+            question: 'Problem on Lempel ziv code for binary sequence.',
+            slide: '[84,86]'
+        },
+        {
+            Lecture: '3',
+            question_no: '7',
+            question: 'Compare Huffman code and lampelziv code.',
+            slide: '85'
+        },
+        {
+            Lecture: '5',
+            question_no: '8',
+            question: 'Defind Brigthness, Contrast, Hue, Saturation',
+            slide: 'JPEG 8-9'
+        },
+        {
+            Lecture: '5',
+            question_no: '9',
+            question: 'What is the goal of image compression',
+            slide: 'JPEG 12'
+        },
+        {
+            Lecture: '5',
+            question_no: '10',
+            question: 'Compare lossless and Lossy compression.',
+            slide: 'JPEG 13'
+        },
+    ];
+}
+export default list;
